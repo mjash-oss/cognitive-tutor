8b0e225da6781560b7afaf3ad40ec1e6a9679d67
Maya Ashley-Martin <mj.ashleymartin@gmail.com>
2025-05-07 17:44:01 -0500

    .venv

---
+++ app/db.py
@@ -0,0 +1,3 @@
+from flask_sqalalchemy import SQALchemy
+
+db = SQLAlchemy() 
---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = 'your-jwt-secret'
