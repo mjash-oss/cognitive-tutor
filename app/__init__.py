c385c5def0d0a7b7e6cb6c3e6dda0b2bb234bc77
Maya Ashley-Martin <mj.ashleymartin@gmail.com>
2025-05-09 19:58:04 -0500

    JWTKEY

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from .db import db 
 from .routes.auth import auth_bp
 from .routes.lessons import lessons_bp
+from flask_jwt_extended import JWTManager 
 
 def create_app():
     app = Flask(__name__)
@@ -10,8 +11,10 @@ def create_app():
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = 'QZ1_Kf3jmqFddXhN82uXfG63bX9JhZUl-nUi3V_ZiHg'
 
     db.init_app(app)
+    jwt = JWTManager(app) 
 
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
     app.register_blueprint(lessons_bp, url_prefix='/api/lessons')
