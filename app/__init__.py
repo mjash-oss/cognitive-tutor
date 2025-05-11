899b4e3736663190c21c7908535e1eb484438bc3
Maya Ashley-Martin <mj.ashleymartin@gmail.com>
2025-05-11 12:37:23 -0500

    WORK

--- app/__init__.py
+++ app/__init__.py
@@ -22,7 +22,7 @@ def create_app():
 
     CORS(app, 
          origins=["https://deft-mooncake-094a90.netlify.app"], 
-         supports_credentials=True,
+         #supports_credentials=True,
          allow_headers=["Content-Type", "Authorization"],
          methods=["GET", "POST", "OPTIONS"])
 
